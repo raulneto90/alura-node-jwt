5bde347ac0029934833a223a6562e61cdc2a69f0
Raul Neto <raul.neto@tecnospeed.com.br>
2022-05-17 13:21:15 -0300

    feat: initial project

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express';
+
+import { AppError } from './errors/AppError';
+import { routes } from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.use(
+  (
+    error: Error,
+    _resquest: Request,
+    response: Response,
+    _next: NextFunction,
+  ) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({ message: error.message });
+    }
+
+    console.log({ error: error.message, stack: error.stack });
+
+    return response.status(500).json({ message: 'Internal Server Error' });
+  },
+);
+
+export { app };
